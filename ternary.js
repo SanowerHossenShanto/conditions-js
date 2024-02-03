96f11814a624fa0bd339e6dc394b5dd78683ecf4
SanowerHossenShanto <ssarker515@gmail.com>
2024-02-03 07:16:43 +0600

    ternary condition added

---
+++ ternary.js
@@ -0,0 +1,10 @@
+let age=18;
+age>=18 ? console.log("Go to for vote"):console.log("You dont vote");
+let apply;
+let isBcs=true ;
+apply= isBcs==true?"You can apply":"You cant do that";
+console.log(apply)
+
+apply= !isBcs?"You can apply":"You cant do that";
+
+console.log(apply)
