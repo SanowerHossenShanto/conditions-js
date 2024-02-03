7371fd2fcbd2795a6e30eff418955e002afa2697
SanowerHossenShanto <ssarker515@gmail.com>
2024-02-03 06:20:27 +0600

    added if else

---
+++ comparition.js
@@ -0,0 +1,18 @@
+// console.log(5>10)
+// console.log(5<10)
+// console.log(5<=10)
+// console.log(5==5)
+// console.log(5>=10)
+var peyaraPrice=100;
+var applePrice=200;
+console.log(peyaraPrice<applePrice)
+console.log(peyaraPrice<=applePrice)
+console.log(peyaraPrice>=applePrice)
+console.log(peyaraPrice==applePrice)
+
+var num=20;
+var num1="20";
+// double equal == cheecked only number 
+console.log("They are equal:",num == num1)
+// === cheecked number and type also 
+console.log("They are equal:",num===num1)
---
+++ ifElse.js
@@ -0,0 +1,36 @@
+const salary=25000;
+const isBCS=true;
+const height=68;
+if(salary>20000 && height>66){
+    console.log("You can marry my daughter")
+}
+else {
+    console.log("Get out Rubbish")
+}
+if(salary>20000 || height>70)
+
+{
+        console.log("You can marry my daughter")
+    }
+    else {
+        console.log("Get out Rubbish")
+    }
+if(salary>200000 || height>70 || isBCS==true)
+
+{
+        console.log("You can marry my daughter")
+    }
+    else {
+        console.log("Get out Rubbish")
+    }
+
+const isBCS1=false;
+
+if(salary>20000 && height>60 || isBCS1==true)
+
+{
+        console.log("You can marry my daughter")
+    }
+    else {
+        console.log("Get out Rubbish")
+    }
